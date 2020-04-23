1d5976e28da389b3fecbbb1cad71f88ce8c79328
Amro Abdelaziz <amr.hamada93@gmail.com>
2020-04-23 00:17:40 +0000

    changed event listner to type input

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,8 +1,8 @@
 $(document).ready(function() {
-  $('#tweet-text').keyup(function () {
+  $('#tweet-text').on('input',function () {
     const text = $(this).val();
     const letterCount = text.trim().length;
-    $(this).parent().children('.counter').html(140 - letterCount);
+    $(this).parent().children('.counter').text(140 - letterCount);
 
     if (140 - letterCount < 0) {
       $(this).parent().children('.counter').addClass('limit-exceeded');
