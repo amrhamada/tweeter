515737f30b535c5e49085749411ce55c3345a2d2
Amro Abdelaziz <amr.hamada93@gmail.com>
2020-04-27 07:29:26 +0000

    added function to calculate stamp of post, and added icons

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -8,8 +8,74 @@
 const textLengthError = '❌ Character Limit exceeded! ❌'
 const emptyError = '❌ Empty Tweet! ❌';
 
-const createTweetElement = function(tweet) {
+//calculate and return the tweet time Stamp
+//if > year than print date posted
+// if > month print how many months
+// if > days return day and if less return hour then minute
+// only one type is return edex "1 month ago" never  "1 month 3 hours 2muntes and 43 seconds ago" is returned
+function dateDiff(startDate, endDate) {
+  const startYear = startDate.getFullYear();
+  const february = (startYear % 4 === 0 && startYear % 100 !== 0) || startYear % 400 === 0 ? 29 : 28;
+  const daysInMonth = [31, february, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+
+  let years = endDate.getFullYear() - startYear;
+  let months = endDate.getMonth() - startDate.getMonth();
+  if (months < 0) {
+      years--;
+      months += 12;
+  }
+  let days = endDate.getDate() - startDate.getDate();
+  if (days< 0) {
+      if (months > 0) {
+          months--;
+      } else {
+          years--;
+          months = 11;
+      }
+      days += daysInMonth[startDate.getMonth()];
+  }
+  let hours = endDate.getHours() - startDate.getHours();
+  let minutes = endDate.getMinutes() - startDate.getMinutes();
+  let seconds = endDate.getSeconds() - startDate.getSeconds();
+
+  if (years > 0) {
+    return startDate.toDateString();
+  }
+  if (months > 0) {
+    if (months === 1 && days < 0) {
+      return `${daysInMonth[startDate.getMonth()] +days} ${daysInMonth[startDate.getMonth()] + days > 1 ? "Days Ago":"Day ago"}`;
+    }
+    return `${Math.abs(months)} ${Math.abs(months) > 1 ? "Months Ago":"Month ago"}`;
+  }
+
+  if (days > 0) {
+    if (days === 1 && hours< 0) {
+      return `${24 + hours} ${24 + hours > 1 ? "Hours Ago":"Hour ago"}`;
+    }
+    return `${Math.abs(days)} ${Math.abs(days) > 1 ? "Days Ago":"Day ago"}`;
+  }
+
+  if (hours > 0) {
+    if (hours === 1 && minutes< 0) {
+      return `${Math.abs(60 + minutes)} ${60 + minutes> 1 ? "Minutes Ago":"Minute ago"}`;
+    }
+    return `${Math.abs(hours)} ${Math.abs(hours) > 1 ? "Hours Ago":"Hour ago"}`;
+  }
+
+  if (minutes > 0) {
+    if (minutes === 1 && seconds< 0) {
+      return `${ 60 +seconds} ${60 + seconds > 1 ? "Seconds Ago":"Second ago"}`;
+    }
+    return `${Math.abs(minutes)} ${Math.abs(minutes) > 1 ? "Minutes Ago":"Minute ago"}`;
+  }
+
+  return `${Math.abs(seconds)} ${Math.abs(seconds) > 1 ? "Seconds Ago":"Second ago"}`;
   
+}
+
+const createTweetElement = function(tweet) {
+  const postedDate = new Date(tweet.created_at);
+  const todaysDate = new Date();  
   const markup = `
     <article>
       <header>
@@ -22,7 +88,12 @@ const createTweetElement = function(tweet) {
       </div>
       <hr>
       <footer>
-        <p>10 Days Ago</p>
+        <p>${dateDiff(postedDate, todaysDate)}</p>
+        <span class="tweet-icons">
+        <img src="./images/flag-icon.png" title="Save" alt="Bookmark tweet">
+        <img src="./images/retweet-icon.png" title="Retweet" alt="retweet tweet">
+        <img src="./images/like-icon.png" title="Like" alt="Like tweet">
+        </span>
       </footer>
     </article>`;
   return markup;
@@ -76,7 +147,7 @@ $(document).ready(function() {
       data: tweet
     }).then( function() {
       loadTweets();
-      $('.new-tweet').slideUp();
+      // $('.new-tweet').slideUp();
     }).catch(error => {
       $('#error').text(' ❌Failed to post please try again❌').slideDown()
       return;
